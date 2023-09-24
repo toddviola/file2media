26a3f8b6ae9735b8610bd03fc7c3372bf2110cec
Todd Viola <todd@violadigital.com>
2023-09-24 13:01:23 -0400

    Initial commit

---
+++ src/Plugin/migrate/source/FileDocument.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\file2media\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Row;
+
+/**
+ * Source plugin for Files (images only).
+ *
+ * @MigrateSource(
+ *   id = "d8_file_document",
+ *   source_provider = "file2media"
+ * )
+ */
+class FileDocument extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    return $this->select('file_managed')
+      ->fields('file_managed', array_keys($this->fields()))
+      ->condition('file_managed.filemime', 'application%', 'LIKE');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'fid' => $this->t('File ID'),
+      'langcode' => $this->t('Langcode'),
+      'uid' => $this->t('User ID'),
+      'filename' => $this->t('File name'),
+      'uri' => $this->t('File URI'),
+      'filemime' => $this->t('File MIME type'),
+      'filesize' => $this->t('File size in bytes'),
+      'status' => $this->t('File status'),
+      'created' => $this->t('File created timestamp'),
+      'changed' => $this->t('File changed timestamp'),
+    ];
+
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'fid' => [
+        'type' => 'integer',
+      ],
+    ];
+  }
+
+}
